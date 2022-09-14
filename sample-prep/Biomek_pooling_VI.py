415a1875ea2bff43451d23c0fb7fbf6467263209
magnulei <Magnus.Leithaug@vetinst.no>
2022-09-14 19:50:49 +0200

    added more info to file + added warning messages

--- sample-prep/Biomek_pooling_VI.py
+++ sample-prep/Biomek_pooling_VI.py
@@ -16,6 +16,17 @@ def main(process_id, file_id):
     lims = Lims(config.BASEURI, config.USERNAME, config.PASSWORD)
     process = Process(lims, id=process_id)
 
+    # Cache all analytes/samples and submitted sample udfs
+
+    analytes = process.all_inputs(unique=True)
+    analytes = lims.get_batch(analytes)
+    lims.get_batch(analyte.samples[0] for analyte in analytes)
+
+    analyte_names = [a.name for a in analytes]
+    species = [a.samples[0].udf["Species"] for a in analytes]
+    genome_size = [a.samples[0].udf["Genome Size (Mbp)"] for a in analytes]
+    project_account = [a.samples[0].udf["Project Account"] for a in analytes]
+
     #load inputs and outputs uri`s`
 
     inputs = []
@@ -31,13 +42,12 @@ def main(process_id, file_id):
 
     #load variables for csv-file
 
-    sample_volumes = [input.udf.get('Sample volume to pool (µl)') for input in inputs]
+    sample_volumes = [round(input.udf.get('Sample volume to pool (µl)'),2) for input in inputs]
     source_location = [input.location for input in inputs]
     total_sample_number = len(sample_volumes)
     Pool_names = [output.name for output in outputs]
     sample_name = [input.name for input in inputs]
-
-    print(sample_name)
+    molarity = [input.udf.get('Molarity TS (nM)') for input in inputs]
 
     #exit script if entered step with more than one pool
 
@@ -51,11 +61,71 @@ def main(process_id, file_id):
     #write csv-file
 
     with open(file_id + "-Biomek_pooling.csv", "w") as file:
-        file.write("LibLabware,LibWellPos,DestLabware,DestWellPos,LibVol" + '\n')
+        file.write("LibLabware,LibWellPos,DestLabware,DestWellPos,LibVol,SampleName,Molarity,Species,GenomeSize,ProjectAccount" + '\n')
         for i in range(total_sample_number):
-            file.write("Libraries" + ',' + str(re.sub(r':','',source_location[i][1])) + ',' + 'LibraryPool'  + ',' + "D1" + "," + str(sample_volumes[i]) + "," + str(sample_name[i]) +'\n')
+            file.write(
+                "Libraries" + ',' + 
+                str(re.sub(r':','',source_location[i][1])) + ',' + 
+                'LibraryPool'  + ',' + 
+                "D1" + "," + 
+                str(sample_volumes[i]) + "," + 
+                str(sample_name[i]) + "," + 
+                str(molarity[i]) + "," +
+                str(species[i]) + "," + 
+                str(genome_size[i]) + "," +
+                str(project_account[i]) + "," + '\n')
     
-    print("Successfully created Biomek csv file for pooling")
+    #print final message. Print warning messages if total volume or individial sample volumes is too high or too small.
+
+    warning1a = False #over 26µl, total volume over 1500 and under 2µl
+    warning1b = False #over 26µl and total volume over 1500
+    for i in range(total_sample_number):
+        if sample_volumes[i] > 26 and sum(sample_volumes) > 1500:
+            for i in range(total_sample_number):
+                if sample_volumes[i] < 2:
+                    warning1a = True
+                    break
+                else:
+                    warning1b = True
+            break
+
+    warning2a = False #sample pooling volumes over 26µl and under 2µl
+    warning2b = False #sample pooling volumes over 26µl
+    for i in range(total_sample_number):
+        if sample_volumes[i] > 26:
+            for i in range(total_sample_number):
+                if sample_volumes[i] < 2:
+                    warning2a = True
+                    break
+                else:
+                    warning2b = True           
+            break
+
+    warning3 = False #volume over 1500. Individual sample volumes ok.
+    for i in range(total_sample_number):
+        if sum(sample_volumes) > 1500:
+            warning3 = True
+            break
+
+    warning4 = False #sample volumes under 2µl
+    for i in range(total_sample_number):
+        if sample_volumes[i] < 2:
+            warning4 = True
+    
+    if warning1a == True:
+        print("WARNING: pooling volume(s) > 26µl and <2µl, while total volume is over 1.5ml. Consider making adjustments")
+    elif warning1b == True:
+        print("WARNING: pooling volume(s) > 26µl and total volume is over 1.5ml. Consider making adjustments")
+    elif warning2a == True:
+        print("WARNING: pooling volume(s) > 26µl and < 2µl. Consider making adjustments")
+    elif warning2b == True:
+        print("WARNING: pooling volume(s) > 26µl. Consider making adjustments")
+    elif warning3 == True:
+        print("WARNING: total volume is over 1.5ml. Consider making adjustments")
+    elif warning4 == True:
+        print("WARNING: pooling volume(s) < 2µl. Consider making adjustments")
+    else:
+        print("Successfully created Biomek csv file for pooling")
 
 main(sys.argv[1], sys.argv[2])
 
